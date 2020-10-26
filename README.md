215b6274fd43f2698dd048036370021a8b216b50
Alexander Hoem Rosbach <ahr@computas.com>
2020-10-26 12:57:40 +0100

    Add nginx ingress controller

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # k8s-infra
+
+1. Create cluster: `./create-cluster.sh`
+2. Setup GitHub token in env: `export GITHUB_TOKEN="$(cat token.txt)"`
+3. Bootstrap gitops-toolkit: `gotk bootstrap github --owner=cx-devops-101 --repository=k8s-infra --path=cluster-production`
---
+++ create-cluster.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# --enable-stackdriver-kubernetes \
+#  --addons HorizontalPodAutoscaling,HttpLoadBalancing \
+
+
+gcloud beta container --project "computas-universitet" clusters create "devops-101" \
+  --zone "europe-north1-a" \
+  --no-enable-basic-auth \
+  --cluster-version "1.18.9-gke.2501" \
+  --release-channel "rapid" \
+  --machine-type "e2-medium" \
+  --image-type "COS" \
+  --disk-type "pd-standard" \
+  --disk-size "100" \
+  --metadata disable-legacy-endpoints=true \
+  --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+  --num-nodes "3" \
+  --enable-ip-alias \
+  --network "projects/computas-universitet/global/networks/default" \
+  --subnetwork "projects/computas-universitet/regions/europe-north1/subnetworks/default" \
+  --default-max-pods-per-node "110" \
+  --no-enable-master-authorized-networks \
+  --enable-autoupgrade \
+  --enable-autorepair \
+  --max-surge-upgrade 1 \
+  --addons HorizontalPodAutoscaling \
+  --max-unavailable-upgrade 0 \
+  --enable-shielded-nodes
